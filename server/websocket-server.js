07b9d03b26ba99fa2ad29c13c4f5fbe9457a3cf8
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2023-08-30 20:32:12 -0600

    Update websocket-server.js added login fuctionality that might work?

--- server/websocket-server.js
+++ server/websocket-server.js
@@ -25,7 +25,6 @@ try {
 } catch (err) {
   console.log('Failed to read maxUsersLog.json, starting a new log.');
 }
-
 const today = new Date().toISOString().split('T')[0];
 let maxUsers = maxUsersLog[today] || 0;
 
@@ -54,3 +53,47 @@ io.on('connection', (socket) => {
 httpsServer.listen(port, () => {
   console.log(`Socket.io server running on port ${port}`);
 });
+
+
+
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+app.get('/', (request, response) => {
+	return response.sendFile('bread.html', { root: '.' });
+});
+
+app.get('/auth/discord', (request, response) => {
+	return response.sendFile('bread_login.html', { root: '.' });
+});
+app.listen(port, () => console.log(`App listening at ${port}`));
+
+window.onload = () => {
+const fragment = new URLSearchParams(window.location.hash.slice(1));
+const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+
+if (!accessToken) {
+    window.location.href('/')
+    return (document.getElementById('login').style.display = 'block');
+}
+
+
+};
+fetch('https://discord.com/api/users/@me', {
+    headers: {
+        authorization: `${tokenType} ${accessToken}`,
+    },
+})
+.then(result => result.json())
+.then(response => {
+    //handle response
+})
+.catch(console.error);
+const { username, discriminator, avatar, id} = response;
+//set the welcome username string
+document.getElementById('name').innerText = ` ${username}#${discriminator}`;
+
+//set the avatar image by constructing a url to access discord's cdn
+document.getElementById("avatar").src = `https://cdn.discordapp.com/avatars/${id}/${avatar}.jpg`;
